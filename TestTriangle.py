ab26d6c36617201be52a4b41d3f788b54dbfe6d5
rpatel1291 <rpatel1291@gmail.com>
2019-09-29 00:26:57 -0400

    PostFix

--- TestTriangle.py
+++ TestTriangle.py
@@ -1,23 +1,33 @@
 import unittest
 import pytest
 
-from Triangle import classifyTraingle, validInput
+from Triangle import classify_triangle, valid_input
+
 
 class TestTriangle(unittest.TestCase):
+
     def testValidInputs(self):
-       self.assertEqual(validInput(-1,2,3), False, "-1,2,3 are invalid inputs for a triangle")
+        self.assertEqual(valid_input(-1, 2, 3), False, "-1,2,3 are invalid inputs for a triangle")
+
     def testValidInputs2(self):
-        self.assertEqual(validInput(1,2,3), False, "1,2,3 are invalid inputs for a triangle")
+        self.assertEqual(valid_input(1, 2, 3), False, "1,2,3 are invalid inputs for a triangle")
+
     def testValidInputs3(self):
-        self.assertEqual(validInput(3,4,5), True, "3,4,5 are valid inputs for a triangle")
+        self.assertEqual(valid_input(3, 4, 5), True, "3,4,5 are valid inputs for a triangle")
+
     def testTriangle1(self):
-        self.assertEqual(classifyTraingle(1,2,3),'NotATriangle', '1,2,3 is not a triangle')
-    def testTrangle2(self):
-        self.assertEqual(classifyTraingle(3,3,3), 'Equilateral', '3,3,3 is a equilateral triangle')
+        self.assertEqual(classify_triangle(1, 2, 3), 'NotATriangle', '1,2,3 is not a triangle')
+
+    def testTriangle2(self):
+        self.assertEqual(classify_triangle(3, 3, 3), 'Equilateral', '3,3,3 is a equilateral triangle')
+
     def testTriangle3(self):
-        self.assertEqual(classifyTraingle(3,4,5), 'RightIsoceles', '3,4,5 is a right and isoceles triangle')
+        self.assertEqual(classify_triangle(3, 4, 5), 'RightIsoceles', '3,4,5 is a right and isoceles triangle')
+
     def testTriangle4(self):
-        self.assertEqual(classifyTraingle(2,2,8**.5), 'RightScalene', '2,2,sqrt(8) is a right and scalene triangle')
+        self.assertEqual(classify_triangle(2, 2, 8 ** .5), 'RightScalene',
+                         '2,2,sqrt(8) is a right and scalene triangle')
+
 
 if __name__ == '__main__':
     unittest.main()
--- Triangle.py
+++ Triangle.py
@@ -1,21 +1,21 @@
-def validInput(a,b,c):
+def valid_input(a, b, c):
     if a <= 0 or b <= 0 or c <= 0:
         return False
-    elif (c < a+b) or (b < c+a) or (a < b+c):
+    elif (c < a + b) or (b < c + a) or (a < b + c):
         return True
     else:
         return False
 
 
-def classifyTraingle(a,b,c):
+def classify_triangle(a, b, c):
     result = 'NotATriangle'
-    if validInput(a,b,c):
+    if valid_input(a, b, c):
         result = ''
         if a == b and c == a:
             result += 'Equilateral'
         elif ((a**2)+(b**2)) == (c**2) or ((a**2)+(c**2)) == (b**2) or ((c**2) + (b**2)) == (a**2):
             result += 'Right'
-        elif (a != b) and (b!=c) and (c!=a):
+        elif (a != b) and (b != c) and (c != a):
             result += 'Isoceles'
         else:
             result += 'Scalene'
